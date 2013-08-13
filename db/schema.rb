5239c0904c9e48052a3e1f33ff985e66ed20835e
Kachina Gosselin <kachina@alum.mit.edu>
2013-08-12 21:55:00 -0700

    Update seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130811225132) do
+ActiveRecord::Schema.define(:version => 20130813011521) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "plan_id"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(:version => 20130811225132) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "studio_id"
+  end
+
+  create_table "instructors", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "plans", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ user.add_role :admin
 user.save
 
 puts 'SETTING UP DEFAULT ACCOUNT'
-account = ACCOUNT.create! :plan_id => 1000, :user_id = 1, :stripe_customer_token => "cus_2LhxTeT79mll1r", :email => "kachina@alum.mit.edu", :is_active => true
+account = Account.create! :plan_id => 1000, :user_id => 1, :stripe_customer_token => "cus_2LhxTeT79mll1r", :email => "kachina@alum.mit.edu", :is_active => true
 
 puts 'SETTING UP DEFAULT STUDIO'
-studio = STUDIO.create! :name => "Test Studio", :location = "29 Rausch Street San Francisco CA 94103", :address => "29 Rausch Street", :city => "San Francisco", :state => "CA"
+studio = Studio.create! :name => "Test Studio", :location => "29 Rausch Street San Francisco CA 94103", :address => "29 Rausch Street", :city => "San Francisco", :state => "CA"
