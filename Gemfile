868cf327facd4052910fa4b9e94d612e04ae54b9
Kachina Gosselin <kachina@alum.mit.edu>
2013-10-31 18:44:40 -0700

    Fix gmaps view - scripts in footer

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '1.9.3'
 gem 'rails', '3.2.13'
 gem 'pg'
 group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,9 @@
 //= require fullcalendar
 //= require gcal
 //= require bootstrap-datetimepicker
-
+//= require underscore
+//= require gmaps4rails/gmaps4rails.base
+//= require gmaps4rails/gmaps4rails.bing
+//= require gmaps4rails/gmaps4rails.googlemaps
+//= require gmaps4rails/gmaps4rails.mapquest
+//= require gmaps4rails/gmaps4rails.openlayers
