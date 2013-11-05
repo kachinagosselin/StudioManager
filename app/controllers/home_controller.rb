8c090a074840008c588858f79061c51e5c4c1b17
Kachina Gosselin <kachina@alum.mit.edu>
2013-11-05 12:01:02 -0800

    Make the site more accessible to users before they need to create an account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -78,11 +78,11 @@ class HomeController < ApplicationController
                       :events => events
                       })
       end
+    end
       respond_to do |format|
           format.html # index.html.erb
           format.json { render json: @user_events.as_json }
       end
-    end
   end
 
   def about 
@@ -100,89 +100,31 @@ class HomeController < ApplicationController
       if user_signed_in?
       @user = current_user
       @locations = Location.near(current_user.profile.address, 25).all
-      if !@locations.present?
-        @locations = Location.all
-      end
-
-      @json = @locations.to_gmaps4rails do |location, marker|
-          studio = location.studio
-          events = studio.events.as_json
-        html = "<div class='row'>
-        <div class='col-lg-6' style='padding-right:30px;'>
-        <h3>#{studio.name} </h3>"
-        @locations = studio.locations
-        if @locations.count == 1
-            html = html +
-            "<p> Address: #{studio.address} #{studio.city}, #{studio.state} </p>"
-        elsif @locations.count > 1 
-            html = html + "<p> Located at the following addresses: </p>" 
-            @locations.each do |loc|
-            html = html + 
-            "<p> #{loc.address} #{loc.city}, #{loc.state} </p>"
-            end
-        end
-        
-        if studio.main_phone.present?
-            html = html +
-            "<p> Phone: #{studio.main_phone} </p>"
+        if !@locations.present?
+          @locations = Location.all
         end
 
-        if studio.website.present?
-            html = html +
-            "<p> Website: <a>#{studio.website}</a></p>"
+        @json = @locations.to_gmaps4rails do |location, marker|
+            studio = location.studio
+            events = studio.events.as_json
+            marker.infowindow render_to_string(:partial => "/studios/display_marker", :locals => {:studio => studio})        
+            marker.json({:id => studio.id,
+                        :name => studio.name,
+                        })
         end
-        
-        if false
-            html = html + 
-            "<p> #{studio.description} </p>"
+      else
+        @json = Location.all.to_gmaps4rails do |location, marker|
+            studio = location.studio
+            marker.infowindow render_to_string(:partial => "/studios/display_marker", :locals => {:studio => studio})        
+            marker.json({:id => studio.id,
+                        :name => studio.name
+                        })
         end
-        
-        html = html +
-        "</div>
-        
-        <div class='col-lg-5' style='margin-top:25px;'>
-        <div class='panel panel-primary' >
-        <div class='panel-heading'>
-        <h5 class='panel-title'>Studio Offerings" 
-        if !studio.packages.present? && !studio.memberships.present?
-          html = html + " - there are none available through this site"
-           html = html + "</h5>
-        </div>"
-        else
-        html = html + "</h5>
-        </div>
-        
-        <ul class='list-group' style='list-style-type:none;'>"
-        
-        studio.memberships.each do |membership|
-            name = membership.title
-            id = membership.id
-            profile_id = @user.profile.id
-            if membership.purchased?(@user.profile)
-            html = html + "
-            <li class='list-group-item'><b>#{name} </b> (Purchased) </li>"
-            else
-            html = html + "
-            <li class='list-group-item'>#{name} <a href='/memberships/#{id}/purchase/#{profile_id}' style='margin-top:-5px;' class='btn btn-primary btn-sm pull-right'>
-            Purchase</a></li>"
-            end
-        end
-        
-        html = html + "</ul>"
-
-        end 
-        html = html + "</div></div></div>"          
-          marker.json({:id => studio.id,
-                      :name => studio.name,
-                      :html => html,
-                      :events => events
-                      })
       end
       respond_to do |format|
           format.html # index.html.erb
-          format.json { render json: @user_events.as_json }
+          format.json 
       end
-    end
   end
 
   def events
